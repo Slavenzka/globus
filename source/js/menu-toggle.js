5c5f1adafd6f380238ecc169079a5f2576344c41
Ivan <proekt.energetika@gmail.com>
2018-10-08 12:17:37 +0300

    Исправил шрифты, мелкие недочеты

--- source/js/menu-toggle.js
+++ source/js/menu-toggle.js
@@ -10,7 +10,6 @@
     navToggle.addEventListener("click", function() {
       if (navMain.classList.contains("header-main__nav--closed")) {
         navMain.classList.remove("header-main__nav--closed");
-        // submenu.style.display = 'none';
         navMain.classList.add("header-main__nav--opened");
       } else {
         navMain.classList.add("header-main__nav--closed");
